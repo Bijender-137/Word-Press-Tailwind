ddf3685722a3b3440a9140a1760e9b65de44f118
Bijender-137 <bijender.k1996@gmail.com>
2023-09-18 16:15:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["inter"],
+        gilroyRegular: ["Gilroy_regular"],
+        gilroyMedium: ["Gilroy_medium"],
+        gilroyBold: ["Gilroy_bold"],
+        gilroyHeavy: ["Gilroy_heavy"],
+        gilroyLight: ["Gilroy_light"],
+        Personal: ["Konexy_personal"],
+      },
+    },
+  },
+  plugins: [],
+};
